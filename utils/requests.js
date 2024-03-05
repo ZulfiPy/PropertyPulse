641915b2fb722d0559a1b33499b865a47c1c0894
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-05 07:26:36 +0200

    requests file

---
+++ utils/requests.js
@@ -0,0 +1,45 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
+// Fetch all properties
+async function fetchPropertiesData() {
+    try {
+        // Handle the case where the domain is not available yet
+        if (!apiDomain) {
+            return [];
+        }
+
+        const res = await fetch(`${apiDomain}/properties`);
+
+        if (!res.ok) {
+            throw new Erroror('Failed to fetch data');
+        }
+
+        return res.json();
+    } catch (Error) {
+        console.log(Error);
+        return [];
+    }
+}
+
+// Fetch signle propertie
+async function fetchProperty(id) {
+    try {
+        if (!apiDomain) return null;
+
+        const res = await fetch(`${apiDomain}/properties/${id}`);
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        return res.json();
+    } catch (Error) {
+        console.log(Error);
+        return null
+    }
+}
+
+export {
+    fetchPropertiesData,
+    fetchProperty
+}
