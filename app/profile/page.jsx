b2da4a1c2e6a95e305896c1f39a24d47c9a336f6
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-07 07:31:03 +0200

    handle delete property feature added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import profileDefault from "@/assets/images/profile.png";
 import Spinner from "@/components/Spinner";
+import {toast} from "react-toastify";
 
 const ProfilePage = () => {
     const { data: session } = useSession();
@@ -40,8 +41,28 @@ const ProfilePage = () => {
 
     }, [session]);
 
-    const handleDeleteProperty = () => {
+    const handleDeleteProperty = async (propertyId) => {
+        const confirmed = window.confirm('Are you sure you want to delete this property?');
 
+        if (!confirmed) return;
+
+        try {
+            const res = await fetch(`/api/properties/${propertyId}`, {
+                method: 'DELETE'
+            });
+
+            if (res.status === 200) {
+                const updatedProperties = properties.filter((property) => property._id !== propertyId);
+
+                setProperties(updatedProperties);
+                toast.success('property deleted');
+            } else {
+                toast.error('Failed to delete property');
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error('failed to delete property');
+        }
     }
 
     return (
