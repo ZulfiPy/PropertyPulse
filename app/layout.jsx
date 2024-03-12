8db3464149e3179be7a4c27f3b55783e430fa63c
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-12 07:58:45 +0200

    global context provider/photoswipe feature

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
-import "@/assets/styles/globals.css";
 import { ToastContainer } from "react-toastify";
+import { GlobalProvider } from "@/context/GlobalContext";
+import "@/assets/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import "photoswipe/dist/photoswipe.css";
 
 export const metadata = {
     title: "PropertyPulse | Find The Perfect Rental",
@@ -13,16 +15,18 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
     return (
-        <AuthProvider>
-            <html>
-                <body>
-                    <Navbar />
-                    <main>{children}</main>
-                    <Footer />
-                    <ToastContainer />
-                </body>
-            </html>
-        </AuthProvider>
+        <GlobalProvider>
+            <AuthProvider>
+                <html>
+                    <body>
+                        <Navbar />
+                        <main>{children}</main>
+                        <Footer />
+                        <ToastContainer />
+                    </body>
+                </html>
+            </AuthProvider>
+        </GlobalProvider>
     );
 }
 
